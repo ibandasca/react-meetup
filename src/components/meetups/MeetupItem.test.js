e9352ee5fb5998212cbd2287116df69b915233da
Iban Dasca <ibandascaibanga@MacBook-Pro-de-Iban.local>
2022-06-18 12:37:25 +0200

    Fix MeetupItem test

--- src/components/meetups/MeetupItem.test.js
+++ src/components/meetups/MeetupItem.test.js
@@ -1,8 +1,12 @@
 /* eslint-disable testing-library/no-debugging-utils */
 import { shallow } from "enzyme";
 import MeetupItem from "./MeetupItem";
+import {MeetupsListProvider} from '../../contexts/meetupListContext'
 
 test("<MeetupItem/> renders without crashing", () => {
-  const wrapper = shallow(<MeetupItem />);
+  const wrapper = shallow(
+  <MeetupsListProvider>
+    <MeetupItem />
+  </MeetupsListProvider>);
   expect(wrapper.exists()).toBe(true);
 });
